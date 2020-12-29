8e29b06062572cc1cac4003768d246672301ab10
Pedro Colombano <pedromecf@gmail.com>
2020-12-29 14:06:43 -0300

    Project created

---
+++ src/test/java/com/colombano/projectTwo/WsSpbootMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.colombano.projectTwo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsSpbootMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
