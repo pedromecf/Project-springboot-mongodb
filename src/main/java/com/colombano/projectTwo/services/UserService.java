0571ca18c6fd0c2c26f62e0b710ba3bf9c8e982e
Pedro Colombano <pedromecf@gmail.com>
2021-01-10 16:25:18 -0300

    Updating user with PUT

--- src/main/java/com/colombano/projectTwo/services/UserService.java
+++ src/main/java/com/colombano/projectTwo/services/UserService.java
@@ -35,6 +35,17 @@ public class UserService {
 		userRepository.deleteById(id);;
 	}
 	
+	public User update(User obj) {
+		User newObj = findById(obj.getId());
+		updateUserData(newObj, obj);
+		return userRepository.save(newObj);
+	}
+	
+	private void updateUserData(User newObj, User obj) {
+		newObj.setName(obj.getName());
+		newObj.setEmail(obj.getEmail());
+	}
+
 	public User fromDTO(UserDTO objDto) {
 		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
