5f3668a704383cada8b59e645734ebcad5f9092b
Pedro Colombano <pedromecf@gmail.com>
2021-01-10 14:28:25 -0300

    Connecting into mongoDB | Repository and Service

--- src/main/java/com/colombano/projectTwo/domain/User.java
+++ src/main/java/com/colombano/projectTwo/domain/User.java
@@ -2,10 +2,15 @@ package com.colombano.projectTwo.domain;
 
 import java.io.Serializable;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection="user") // collection is optional to name a document collection;
 public class User implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
 	
+	@Id
 	private String id;
 	private String name;
 	private String email;
