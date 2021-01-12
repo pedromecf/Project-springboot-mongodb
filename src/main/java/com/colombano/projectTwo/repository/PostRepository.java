f1c6ebde073a4247396d81f7e22aa30ee9341cab
Pedro Colombano <pedromecf@gmail.com>
2021-01-12 19:29:18 -0300

    Simple search using Query

--- src/main/java/com/colombano/projectTwo/repository/PostRepository.java
+++ src/main/java/com/colombano/projectTwo/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.colombano.projectTwo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.colombano.projectTwo.domain.Post;
@@ -11,4 +12,8 @@ import com.colombano.projectTwo.domain.Post;
 public interface PostRepository extends MongoRepository<Post, String>{
 	
 	List<Post> findByTitleContainingIgnoreCase(String title);
+	
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String title); // Custom method
+	
 }
--- src/main/java/com/colombano/projectTwo/services/PostService.java
+++ src/main/java/com/colombano/projectTwo/services/PostService.java
@@ -22,7 +22,7 @@ public class PostService {
 	}
 		
 	public List<Post> findByTitle(String title) {
-		return postRepository.findByTitleContainingIgnoreCase(title);
+		return postRepository.searchTitle(title);
 	}
 	
 }
